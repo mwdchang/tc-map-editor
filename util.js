391810b97cf415d80003e9afc3093e463da48d9b
Daniel <mwdchang@gmail.com>
2017-01-31 00:17:49 -0500

    import/export

--- util.js
+++ util.js
@@ -1,3 +1,19 @@
+function bindCells(target, data, cfg) {
+  canvas.selectAll('.cell').remove();
+  canvas.selectAll('.cell')
+    .data(data)
+    .enter()
+    .append('rect')
+    .classed('cell', true)
+    .attr('x', d => d.x*cfg.w)
+    .attr('y', d => d.y*cfg.w)
+    .attr('width', cfg.w)
+    .attr('height', cfg.h)
+    .style('fill', '#FFF')
+    .style('stroke', 'none');
+
+}
+
 function initData(size) {
   let data = [];
   for (let i=0; i < size; i++) {
@@ -10,12 +26,12 @@ function initData(size) {
   return data;
 }
 
-function YGrid(target, gridSize, width) {
-  for (let x=0; x <= gridSize; x++) {
+function YGrid(target, cfg) {
+  for (let x=0; x <= cfg.gridSize; x++) {
       target.append('line')
-        .attr('x1', x*width)
+        .attr('x1', x*cfg.w)
         .attr('y1', 0)
-        .attr('x2', x*width)
+        .attr('x2', x*cfg.w)
         .attr('y2', 200)
         .style('pointer-events', 'none')
         .style('stroke', '#CCC')
@@ -23,13 +39,13 @@ function YGrid(target, gridSize, width) {
   }
 }
 
-function XGrid(target, gridSize, height) {
-  for (let y=0; y <= gridSize; y++) {
+function XGrid(target, cfg) {
+  for (let y=0; y <= cfg.gridSize; y++) {
       target.append('line')
         .attr('x1', 0)
-        .attr('y1', y*height)
+        .attr('y1', y*cfg.h)
         .attr('x2', 200)
-        .attr('y2', y*height)
+        .attr('y2', y*cfg.h)
         .style('pointer-events', 'none')
         .style('stroke', '#CCC')
         .style('stroke-width', 0.5);
@@ -45,4 +61,23 @@ function exportData(data, size) {
   append(size+','+size);
   data.forEach(appendCell);
   return buffer;
+}
+
+
+function importData(txt) {
+  console.log(txt);
+  let lines = txt.split(/\n/).map(d => d.trim());
+  let size = +lines.shift().split(/,/)[0];
+  let data = initData(size);
+  lines.forEach(line => {
+    if (line === '') return;
+    let components = line.split(/,/);
+    let x = +components[0];
+    let y = +components[1];
+    let h = components[2];
+
+    data[x*size + y].h = h;
+  });
+
+  return { gridSize: size, data: data };
 }
