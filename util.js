80bc1b534fba6f3393c43ebd529c3c910bf8bc0d
Dixteel <twchang2005@gmail.com>
2017-02-26 00:55:56 +0800

    - add resource and zone export. - use different shape.

--- util.js
+++ util.js
@@ -19,7 +19,7 @@ function initData(size) {
   for (let i=0; i < size; i++) {
     for (let j=0; j < size; j++) {
       data.push({
-        x: i, y: j, h: 0, t: 0
+        x: i, y: j, h: 0, t: 0, r: 0, z: 0
       })
     }
   }
@@ -56,7 +56,7 @@ function exportData(data, size) {
   let buffer = '';
   let append = (str) => { buffer = buffer + str; buffer = buffer + '\n'};
   let appendCell = (cell) => {
-    append(cell.x + ',' + cell.y + ',' + cell.h + ',' + cell.t);
+    append(cell.x + ',' + cell.y + ',' + cell.h + ',' + cell.t + ',' + cell.r + ',' + cell.z);
   };
   append(size+','+size);
   data.forEach(appendCell);
@@ -76,9 +76,13 @@ function importData(txt) {
     let y = +components[1];
     let h = components[2];
     let t = components[3];
+	let r = components[4];
+	let z = components[5];
 
     data[x*size + y].h = h;
     data[x*size + y].t = t;
+	data[x*size + y].r = r;
+	data[x*size + y].z = z;
   });
 
   return { gridSize: size, data: data };
